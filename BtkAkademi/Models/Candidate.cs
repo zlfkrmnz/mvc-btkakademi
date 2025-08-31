3a8ed941a73b74a83957a180c257b4aa3127a53b
Zülfükar Minaz <95717508+zlfkrmnz@users.noreply.github.com>
2025-09-01 02:26:45 +0300

    MVC'ye genel bi bakış atıldı.

---
+++ BtkAkademi/Models/Candidate.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        [Required(ErrorMessage = "E-Mail is required.")]
+        public string? Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Firstname is required.")]
+        public string? FirstName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Lastname is required.")]
+        public string? LastName { get; set;} = string.Empty;
+        public string? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public string? SelectedCourse {  get; set; } = string.Empty;
+        public DateTime ApplyAt { get; set; } = DateTime.Now;
+    }
+}
