032ea61cbe02a8038c4736273e643d15619fd5eb
Zülfükar Minaz <95717508+zlfkrmnz@users.noreply.github.com>
2025-09-01 00:27:16 +0300

    Temeller atıldı

---
+++ Basics/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using Basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index1()
+        {
+            string message = $"Hello Selin. {DateTime.Now.ToString()}";
+            return View("Index1", message);
+        }
+
+        public ViewResult Index2()
+        {
+            var names = new String[]
+            {
+                "Selin",
+                "Zülfükar",
+                "Berk"
+            };
+            return View(names);
+        }
+
+        public IActionResult Index3()
+        {
+            var list = new List<Employee>()
+            {
+                new Employee() {Id=1, FirstName="Selin", LastName="Altuntaş", Age=22 },
+                new Employee() {Id=2, FirstName="Zülfükar", LastName="Minaz", Age=24 },
+                new Employee() {Id=3, FirstName="Berk", LastName="İçli", Age=22 },
+            };
+            return View(list);
+        }
+    }
+}
---
+++ Basics/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Basics.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName.ToUpper()}";
+        public int Age { get; set; }
+    }
+}
