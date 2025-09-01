424bbbdb58b1eb2cca798663b30ab55f4e3ad0f8
Zülfükar Minaz <95717508+zlfkrmnz@users.noreply.github.com>
2025-09-02 01:45:45 +0300

    Veri-Odaklı Geliştirme yapıldı.

---
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _repositoryContext;
+        public ProductController(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+        public IActionResult Index()
+        {
+            var model = _repositoryContext.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Get(int id)
+        {
+            Product product = _repositoryContext.Products.First(p => p.ProductId == id);
+            return View(product);
+        }
+    }
+}
